489642d2581f8b6ff1230a21c6c317d47030f461
congenial-principle-5422 <christinajenkins10@gmail.com>
2020-03-18 03:37:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
 var array = [];
-var action = 'plays';
+var action = " " + 'plays';
 for (var i = 0; i < musicians.length; i++) {
   array.push(musicians[i] + instruments[i]);}
   return theBeatlesPlay = array
