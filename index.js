6372ef6f48d22464ce7c1744c47de45069c42822
congenial-principle-5422 <christinajenkins10@gmail.com>
2020-03-18 03:20:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+for (let musicians = intruments) {
+  console.log( {musicians} "plays" {instruments} );
+  }
+}
+ theBeatlesPlay(musicians, intruments)
