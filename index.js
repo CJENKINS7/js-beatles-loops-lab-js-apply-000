8ffadbbd5785e4d62e50769972cebf767859b43d
congenial-principle-5422 <christinajenkins10@gmail.com>
2020-03-18 03:21:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-function theBeatlesPlay(musicians, instruments){
-
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-
-for (let musicians = intruments) {
-  console.log( {musicians} "plays" {instruments} );
+function theBeatlesPlay(musicians, instruments) {
+var array = [];
+var action = 'plays'
+for (var i = 0; i < musicians.length; i++) {
+  array.push(musicians[i] + action + instruments[i]);
   }
+  return array
 }
- theBeatlesPlay(musicians, intruments)
+
+
+function johnLennonFacts(facts) {
+  var Lennonfacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    Lennonfacts.push(facts[i] + "!!!");
+    i++;
+  }
+  return Lennonfacts;
+}
+
+function iLoveTheBeatles(n){
+var array = [];
+do {
+  array.push('I love the Beatles!')
+  n++;
+}  while (n < 15);
+return array;
+}
+//n = number of times to say 'I love the Beatles'
+//n = 15
+//n < needs to be less than 15 to help return array
+
